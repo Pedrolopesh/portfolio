48accd1113d17ffded5f4b10f7d5221563638c1a
PHW <pedrolopeshls99@gmail.com>
2019-12-15 12:24:55 -0300

    new archaives

---
+++ src/components/cpmMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+<b-container class="bv-example-row mt-10">
+  <b-row align-h="end">
+  <b-button>home</b-button>
+  <b-button>Button</b-button>
+  <b-button>Button</b-button>
+  <b-button>Button</b-button>
+  </b-row>
+</b-container>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  components: {},
+  created() {},
+  data: () => ({}),
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+
+    <div class="bg-full-screen bv-example-row container-header">
+    
+    <b-col><cpmLogo></cpmLogo></b-col>
+    <b-col><sideMenu></sideMenu></b-col>
+      
+    </div>
+
+
+  
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import sideMenu from '../components/cpmMenu.vue'
+import cpmLogo from '../components/cpmLogo.vue'
+// import axios from 'axios'
+
+
+export default {
+  name: 'Home',
+
+  components: {
+    cpmLogo,
+    sideMenu
+  },
+  created() {},
+  data: () => ({}),
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
