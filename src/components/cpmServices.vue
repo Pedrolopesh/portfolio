ad9070a72c409b15ec59819365cc31b4708d1e1b
PHW <pedrolopeshls99@gmail.com>
2019-12-30 14:21:47 -0300

    new layout for services

--- src/components/cpmServices.vue
+++ src/components/cpmServices.vue
@@ -1,62 +1,59 @@
 <template>
   <div class="mt-8">
-      <div class="container-service ac mt-10">
-          <!-- <b-container class="bv-example-row"> -->
 
-        <b-row>
-            <b-col cols="8" class="border-text container-content-text">
-                <p class="content-text">
-                    Produção e elaboração de artes 
-                </p>
-            </b-col>
-            <b-col cols="4">
-                <div class="bg-image-service-1">
-                    <!-- <img src="../assets/src/imagens/contact.jpg"> -->
+<div class="container-service container">
+
+    <b-row>
+      <b-col align-self="start">
+        <div class="container-card">
+            <div class="bg-image-card">
+                <div class="bg-mask">
+                    <h1 class="content-card">
+                        Criação e elaboração de artes gráficas para marketing e brandings
+                    </h1>
                 </div>
-            </b-col>
-        </b-row>
-        <!-- </b-container> -->
-      </div>
+            </div>
+        </div>
+      </b-col>
+      
+    </b-row>
+</div>
 
+<div class="container-service container mt-5">
 
-
-            <div class="container-service ac mt-10">
-          <!-- <b-container class="bv-example-row"> -->
-        <b-row>
-            <b-col class="mr-5">
-                <div class="bg-image-service-2">
-                    <!-- <img src="../assets/src/imagens/contact.jpg"> -->
+    <b-row>
+      <b-col align-self="center">
+        <div class="container-card ac">
+            <div class="bg-image-card ac">
+                <div class="bg-mask">
+                    <h1 class="content-card">
+                        Desenvolvimento de web sites para visualização e interação com usuário
+                    </h1>
                 </div>
-            </b-col>
-            <b-col class="border-text container-content-text">
-                <p class="content-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quo blanditiis placeat deserunt deleniti pariatur, id obcaecati quisquam? Id voluptas tenetur modi velit dolorem, asperiores molestiae. Vel sed eos consequatur exercitationem iusto facilis. Non exercitationem quia amet, cupiditate quisquam vel voluptate! Non commodi sint necessitatibus quos alias dolor at doloribus.
-                </p>
-            </b-col>
+            </div>
+        </div>
+      </b-col>
+      
+    </b-row>
+</div>
 
-        </b-row>
-        <!-- </b-container> -->
-      </div>
+<div class="container-service container mt-5">
 
-
-
-            <div class="container-service ac mt-10">
-          <!-- <b-container class="bv-example-row"> -->
-        <b-row>
-            <b-col class="border-text mr-5 container-content-text">
-                <p class="content-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quo blanditiis placeat deserunt deleniti pariatur, id obcaecati quisquam? Id voluptas tenetur modi velit dolorem, asperiores molestiae. Vel sed eos consequatur exercitationem iusto facilis. Non exercitationem quia amet, cupiditate quisquam vel voluptate! Non commodi sint necessitatibus quos alias dolor at doloribus.
-                </p>
-            </b-col>
-            <b-col>
-                <div class="bg-image-service-3">
-                    <!-- <img src="../assets/src/imagens/contact.jpg"> -->
+    <b-row>
+      <b-col align-self="end">
+        <div class="container-card ml-a">
+            <div class="bg-image-card ml-a">
+                <div class="bg-mask">
+                    <h1 class="content-card">
+                        Desenvolvimento de conteúdos para web
+                    </h1>
                 </div>
-            </b-col>
-        </b-row>
-        <!-- </b-container> -->
-      </div>
-
+            </div>
+        </div>
+      </b-col>
+      
+    </b-row>
+</div>
 
 
   </div>
