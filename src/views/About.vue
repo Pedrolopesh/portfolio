432e71605136ed798002b15f77c9f2f42077a643
Pedro Lopes <pedrolopesgls99@gmail.com>
2020-01-08 00:13:55 -0300

    finshing skills

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,8 +5,10 @@
     <div class="bg-mask">
 
 
-    <b-col><cpmLogo></cpmLogo></b-col>
-    <b-col><sideMenu></sideMenu></b-col>
+    <b-col class="container-header">
+      <cpmLogo></cpmLogo>
+      <sideMenu></sideMenu>
+    </b-col>
       
       <div class="container text-container-home">
         <h1 class="tile-home">A portfolio created <br> by <br> Pedro Lopes</h1>
@@ -26,7 +28,13 @@
 
   <div class="container-personal">
     <personal></personal>
+    <!-- <skills></skills> -->
+    <div class="container">
+      <h1 class="title-1">Skills</h1>
+      <p class="content-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur consequuntur nisi impedit aspernatur natus deleniti fuga. Vero, tempore. Maxime, doloribus! Cupiditate ipsum libero, amet illo maiores repudiandae dolores animi ratione provident est at ducimus molestias pariatur. Quae, alias. Id laborum fugiat necessitatibus sed doloribus ducimus, repellendus optio distinctio provident similique!</p>
+    </div>
     <skills></skills>
+    
   </div>
 
 <div class="mt-contact">
@@ -43,7 +51,8 @@
 import sideMenu from '../components/cpmMenu.vue'
 import cpmLogo from '../components/cpmLogo.vue'
 import personal from '../components/cpmPersonal.vue'
-import skills from '../components/cpmSkills.vue'
+// import skills from '../components/cpmSkills.vue'
+import skills from '../components/cpmSkills2.vue'
 import contact from '../components/cpmContact.vue'
 import foot from '../components/cpmFoot.vue'
 // import axios from 'axios'
