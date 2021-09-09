4a3aae6cddf902a5347fe94e49551ea6aa80141e
pedrolopeshls <pedrolopeshls99@gmail.com>
2021-09-09 01:12:58 -0300

    missing the effects transitions for modal

--- personal-page/src/components/ModalWork/index.tsx
+++ personal-page/src/components/ModalWork/index.tsx
@@ -1,6 +1,51 @@
-import { BGModalWork, Container, CloseContainer, CardContainer, ContainerSlickWorks } from './style';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { useEffect, useState } from 'react';
+import { 
+    BGModalWork, 
+    Container, 
+    CloseContainer, 
+    CardContainer, 
+    ContainerSlickWorks,
+    ContainerWorkItem,
+    WorkItem,
+    WorkImage,
+    WorkBoxItem,
+    UnderScoreMarker,
+} from './style';
 
 const ModalWork = () => {
+
+    const [workItens, setWorkItens] = useState([])
+    const [selectedWork, setSelectedWork] = useState(0)
+
+    useEffect(() => {
+
+        const itens:any = [
+            {
+                image:'https://res.cloudinary.com/publi-node-uploads/image/upload/v1631142564/portfolio/pet-care-logo_bxlqea.png',
+                title:'',
+                description:''
+            },
+            {
+                image:'https://res.cloudinary.com/publi-node-uploads/image/upload/v1631142564/portfolio/macht-coder-logo_e2hfmn.png',
+                title:'',
+                description:''
+            },
+            {
+                image:'https://res.cloudinary.com/publi-node-uploads/image/upload/v1631142564/portfolio/alert-commuty-logo_sxzxzf.png',
+                title:'',
+                description:''
+            },
+            {
+                image:'https://res.cloudinary.com/publi-node-uploads/image/upload/v1631142564/portfolio/educaCred-logo_bahqvy.png',
+                title:'',
+                description:''
+            },
+        ]
+
+        setWorkItens(itens)
+    }, [])
+
     return (
         <>
             <BGModalWork>
@@ -10,7 +55,37 @@ const ModalWork = () => {
                     <CardContainer>
                         <ContainerSlickWorks>
 
+                            <ContainerWorkItem 
+                                style={{ transform: ( (selectedWork === 2) ? 'translateX(-180px)' : '') || ((selectedWork === 3) ? 'translateX(-183px)' : ''), }}
+                            >
+                                {
+                                    workItens && 
+                                    (
+                                        <> 
+                                            {workItens.map((item:any, index:number) => {
+                                                return (
+                                                    <>
+                                                        <WorkItem onClick={() => {setSelectedWork(index)}}>
+                                                            <WorkImage src={item.image} alt="work Image" />
+                                                        </WorkItem>
+                                                    </>
+                                                )
+                                            })}
+                                        </>
+                                    ) 
+                                }
+
+                                
+                            </ContainerWorkItem>
+
+                        <UnderScoreMarker
+                            style={{marginLeft: ((selectedWork === 1 || selectedWork === 2) ? 190 : '') || (selectedWork === 3 ? 367 : '')}} 
+                        />
                         </ContainerSlickWorks>
+                        
+                        <WorkBoxItem>
+
+                        </WorkBoxItem>
                     </CardContainer>
                 </Container>
             </BGModalWork>
--- personal-page/src/components/ModalWork/style.tsx
+++ personal-page/src/components/ModalWork/style.tsx
@@ -19,6 +19,9 @@ export const Container = styled.div`
     display: block;
     padding: 10px;
     border-radius: 10px;
+    top: 15%;
+    left: 35%;
+    position: absolute;
 `
 
 export const CloseContainer = styled.div`
@@ -41,10 +44,63 @@ export const CardContainer = styled.div`
     border-radius: 10px;
     max-width: 100%;
     width: 100%;
-    height: 200px;
+    height: 440px;
+`
+export const ContainerSlickWorks = styled.div`
+    display: grid;
+    padding: 10px 0px 0px 10px;
+    overflow: hidden;
+    max-width: 100%;
+    width: 96.6%;
+    /* position: absolute; */
+
+    /* overflow: auto;
+    overflow-x: hidden;
+    overflow-y: hidden; */
 `
 
-export const ContainerSlickWorks = styled.div`
+export const ContainerWorkItem = styled.div`
+    display: flex;
+    max-width: 100%;
+    width: 100%;
+    transition: 1s;
+    /* transform: translateX(10px); */
+`
+
+export const WorkItem = styled.div`
+    display: flex;
+    max-width: 100%;
+    width: 160px;
+    height: 160px;
+    margin: 0px 10px 0px 10px;
+    align-items: center;
+    background-color: white;
+    border-radius: 10px 10px 0px 0px;
+`
+
+export const WorkImage = styled.img`
     display: block;
-    
+    max-width: 100%;
+    width: 90%;
+    margin: 0px auto 0px auto;
+`
+
+export const UnderScoreMarker = styled.span`
+    background-color: #ffffff;
+    display: block;
+    max-width: 100%;
+    height: 10px;
+    width: 160px;
+    margin: 0px 10px 0px 10px;
+    transition: 1s;
+`
+
+export const WorkBoxItem = styled.div`
+    background-color: white;
+    display: block;
+    max-width: 100%;
+    height: 100px;
+    width: 93%;
+    margin: 0px 10px 0px 20px;
+    border-radius: 0px 0px 10px 10px;
 `
